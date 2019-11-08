0a91ca9d4ffb3dae7647204d300724f854e238ea
pieromarini <piero.marini21@gmail.com>
2019-11-08 00:35:38 -0500

    Added structure and base class for testing endpoints

---
+++ src/test/java/com/easyexam/test/AuthenticationTest.java
@@ -0,0 +1,17 @@
+package com.easyexam.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AuthenticationTest {
+
+  @Test
+  public void testLogin() throws Exception {}
+
+  @Test
+  public void testRegister() throws Exception {}
+}

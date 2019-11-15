ca3189f5676e551123a8fd8e3d0576727f231d69
pieromarini <piero.marini21@gmail.com>
2019-11-15 12:31:42 -0500

    Added Facade Class

---
+++ src/main/java/com/easyexam/security/utils/AuthenticationUtils.java
@@ -0,0 +1,29 @@
+package com.easyexam.security.utils;
+
+import com.easyexam.model.User;
+import com.easyexam.security.service.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class AuthenticationUtils {
+
+  @Autowired UserDetailsServiceImpl userDetailsService;
+
+  public String getAuthenticatedUserEmail() {
+    Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    if (principal instanceof UserDetails) {
+      return ((UserDetails) principal).getUsername();
+    } else {
+      return principal.toString();
+    }
+  }
+
+  public User getUserObject() {
+    String email = this.getAuthenticatedUserEmail();
+
+    User user = userDetailsService.getUserFromEmail(email);
+
+    return user;
+  }
+}

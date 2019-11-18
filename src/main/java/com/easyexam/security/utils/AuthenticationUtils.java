ef0a2a02f1ff0bcdaa07d7486f8aa831910391bf
pieromarini <piero.marini21@gmail.com>
2019-11-17 19:32:33 -0500

    fixed dependecy injection bug.

--- src/main/java/com/easyexam/security/utils/AuthenticationUtils.java
+++ src/main/java/com/easyexam/security/utils/AuthenticationUtils.java
@@ -5,7 +5,9 @@ import com.easyexam.security.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 
+@Component
 public class AuthenticationUtils {
 
   @Autowired UserDetailsServiceImpl userDetailsService;
